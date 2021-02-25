3f915ba14693e291aaad8d50c76fed199441bc55
Luis <LVentura335@Gmail.com>
2021-02-25 06:34:08 -0800

    edited hash_func and insert functions

--- HashTable.py
+++ HashTable.py
@@ -13,7 +13,7 @@ class HashTable:
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
   def create_arr(self, size):
-    new_array = []
+    new_array = [None] * size
     for number in range(size):
       new_array[number] = LinkedList()
     return new_array
@@ -26,7 +26,12 @@ class HashTable:
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
   def hash_func(self, key):
-    pass
+    h = 0
+    # Goes through each character in the given key, returns the mod result of the sum of the key's value and the size of the table
+    for char in key:
+      h += ord(char)
+    return (h % self.size)
+
 
 
   # 3️⃣ TODO: Complete the insert method.
@@ -34,13 +39,8 @@ class HashTable:
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    frequency = {}
-    for item in key:
-      if (item in frequency):
-        frequency[item] += 1
-      else:
-        frequency[item] = 
-    pass
+    h = self.hash_func(key)
+    self.arr[h] = value
 
 
 
@@ -57,6 +57,8 @@ class HashTable:
   # erase: 2
 
   def print_key_values(self):
+    for i in self.arr:
+      print(i)
     pass
 
 
